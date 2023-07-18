ce6cfb21f350be448323198a8f09266b17af9a93
张立志 <zhanglizhi@tedu.cn>
2023-07-18 16:48:48 +0800

    密钥安全

--- social-contact/contact-main-spring-boot/src/main/java/com/sparrow/chat/boot/controller/ContactHealthController.java
+++ social-contact/contact-main-spring-boot/src/main/java/com/sparrow/chat/boot/controller/ContactHealthController.java
@@ -2,6 +2,8 @@ package com.sparrow.chat.boot.controller;
 
 import com.sparrow.datasource.DataSourceValidChecker;
 import com.sparrow.datasource.checker.ConnectionValidCheckerAdapter;
+import com.sparrow.protocol.constant.Constant;
+import com.sparrow.support.web.ServletUtility;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletRequest;
 import javax.sql.DataSource;
 
 @RestController
@@ -31,7 +34,11 @@ public class ContactHealthController {
     }
 
     @RequestMapping("env/{env}")
-    public String env(@PathVariable("env") String env) {
-        return System.getenv(env);
+    public String env(@PathVariable("env") String env, ServletRequest servletRequest) {
+        String ip = ServletUtility.getInstance().getClientIp(servletRequest);
+        if (Constant.LOCALHOST_127.equals(ip)) {
+            return System.getenv(env);
+        }
+        return "非常访问";
     }
 }
