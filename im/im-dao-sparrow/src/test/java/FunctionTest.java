f3b57f12b2ae841ea610fd079385c31f147782ec
zh_harry <zh_harry@163.com>
2025-03-14 00:00:34 +0800

    modify websocket

--- im/im-dao-sparrow/src/test/java/FunctionTest.java
+++ im/im-dao-sparrow/src/test/java/FunctionTest.java
@@ -1,18 +1,39 @@
 import com.sparrowzoo.chat.dao.sparrow.dao.po.Session;
 
+import javax.persistence.Column;
+import java.io.Serializable;
+import java.lang.invoke.SerializedLambda;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.function.Function;
 
+@FunctionalInterface
+interface SerializableFunction<T, R> extends Function<T, R>, Serializable {}
+
 public class FunctionTest {
     // 接收方法引用作为参数
-    public static void processString(Function<Session, String> function, String input) {
-        Session session = new Session();
-        String result = function.apply(session);
-        System.out.println(result);
+    public static void processString(SerializableFunction<Session, String> function, String input) {
+        System.out.println(getMethodName(function));
+    }
+
+    public static String getMethodName(Function<?, ?> function) {
+        try {
+            // 反射获取 writeReplace 方法
+            Method method = function.getClass().getDeclaredMethod("writeReplace");
+            method.setAccessible(true);
+            // 调用该方法获取 SerializedLambda 对象
+            SerializedLambda serializedLambda = (SerializedLambda) method.invoke(function);
+
+            // 解析方法名
+            String methodName = serializedLambda.getImplMethodName();
+            System.out.println(serializedLambda.getImplClass());
+            return methodName;
+        } catch (Exception e) {
+            throw new RuntimeException("无法解析方法名", e);
+        }
     }
 
     public static void main(String[] args) {
         processString(Session::getSessionKey, "hello world");
-        new ArrayList<Session>().stream().map(Session::getSessionKey).forEach(System.out::println);
     }
 }
