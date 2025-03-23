14947ed6247e78465e7fad0f1e73506c965375f0
张立志 <zhanglizhi@tedu.cn>
2025-03-23 12:35:35 +0800

    add monitor

--- social-contact/starter.sh
+++ social-contact/starter.sh
@@ -1,4 +1,4 @@
 systemctl daemon-reload  #(重新reload开机启动所有配置)
 systemctl enable supervisord #（允许supervisord服务开机自启动）
-systemctl start supervisord #（启动supervisord）
+systemctl restart supervisord #（启动supervisord）
 systemctl status supervisord #（查看状态）
