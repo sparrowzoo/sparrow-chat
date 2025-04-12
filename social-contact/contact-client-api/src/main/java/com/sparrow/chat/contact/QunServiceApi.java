2fd129a836b9011996aa9192a19b8bd40708b82d
张立志 <zhanglizhi@tedu.cn>
2025-04-12 23:20:23 +0800

    merge

---
+++ social-contact/contact-client-api/src/main/java/com/sparrow/chat/contact/QunServiceApi.java
@@ -0,0 +1,9 @@
+package com.sparrow.chat.contact;
+
+import com.sparrow.protocol.BusinessException;
+
+import java.util.List;
+
+public interface QunServiceApi {
+    List<Long> getMemberById(Long qunId) throws BusinessException;
+}
