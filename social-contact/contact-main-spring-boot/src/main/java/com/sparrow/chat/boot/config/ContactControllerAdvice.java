3b159677c0791877cd9a0c27a57e24b7206cfba2
张立志 <zhanglizhi@tedu.cn>
2025-04-17 21:56:23 +0800

    add test

--- social-contact/contact-main-spring-boot/src/main/java/com/sparrow/chat/boot/config/ContactControllerAdvice.java
+++ social-contact/contact-main-spring-boot/src/main/java/com/sparrow/chat/boot/config/ContactControllerAdvice.java
@@ -3,6 +3,6 @@ package com.sparrow.chat.boot.config;
 import com.sparrow.spring.starter.advice.ControllerResponseAdvice;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(basePackages = {"com.sparrow.chat"})
+@RestControllerAdvice(basePackages = {"com.sparrow.chat","com.sparrow.file"})
 public class ContactControllerAdvice extends ControllerResponseAdvice {
 }
