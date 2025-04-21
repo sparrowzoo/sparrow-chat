16d19648f92f557c12904a787eaa96eba6361345
张立志 <zhanglizhi@tedu.cn>
2025-04-21 14:11:40 +0800

    modify sql

---
+++ social-contact/contact-main-spring-boot/release-node/20250421.sql
@@ -0,0 +1,7 @@
+RENAME TABLE user TO t_user;
+RENAME TABLE audit TO t_audit;
+RENAME TABLE contact TO t_contact;
+RENAME TABLE message TO t_message;
+RENAME TABLE session TO t_session;
+RENAME TABLE qun TO t_qun;
+RENAME TABLE qun_member TO t_qun_member;
