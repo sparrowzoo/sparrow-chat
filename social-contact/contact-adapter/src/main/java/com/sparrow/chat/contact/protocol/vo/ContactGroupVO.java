1015b0cd312268374895cfd67f3b407d8175184e
张立志 <zhanglizhi@tedu.cn>
2025-04-30 10:18:03 +0800

    modify sql

--- social-contact/contact-adapter/src/main/java/com/sparrow/chat/contact/protocol/vo/ContactGroupVO.java
+++ social-contact/contact-adapter/src/main/java/com/sparrow/chat/contact/protocol/vo/ContactGroupVO.java
@@ -1,15 +1,26 @@
 package com.sparrow.chat.contact.protocol.vo;
 
 import com.sparrow.protocol.VO;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 @Data
-@AllArgsConstructor
 public class ContactGroupVO implements VO {
+    public ContactGroupVO(Map<Long, ContactVO> userMap, List<QunVO> quns, List<Long> contactIds) {
+        this.userMap = userMap;
+        this.quns = quns;
+        this.contactIds = contactIds;
+        if (contactIds == null) {
+            this.contactIds = Collections.emptyList();
+        }
+        if (userMap == null) {
+            this.userMap = Collections.emptyMap();
+        }
+    }
+
     private Map<Long, ContactVO> userMap;
     private List<QunVO> quns;
     private List<Long> contactIds;
