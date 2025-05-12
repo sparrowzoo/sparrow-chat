b12cc8a42e1d6a3b0cf0a81c42e8b2c575b4bedd
张立志 <zhanglizhi@tedu.cn>
2025-05-12 17:18:18 +0800

    modify sql

--- im/im-domain/src/main/java/com/sparrow/chat/domain/service/MessageService.java
+++ im/im-domain/src/main/java/com/sparrow/chat/domain/service/MessageService.java
@@ -5,11 +5,13 @@ import com.sparrow.chat.domain.repository.SessionMateRepository;
 import com.sparrow.chat.domain.repository.SessionRepository;
 import com.sparrow.chat.protocol.query.SessionQuery;
 import com.sparrow.concurrent.SparrowThreadFactory;
+import com.sparrow.core.spi.ApplicationContext;
 import com.sparrow.passport.api.UserProfileAppService;
 import com.sparrow.passport.protocol.dto.UserProfileDTO;
 import com.sparrow.protocol.BusinessException;
 import com.sparrow.protocol.LoginUser;
 import com.sparrow.protocol.ThreadContext;
+import com.sparrow.support.AuthenticatorConfigReader;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.inject.Inject;
@@ -34,7 +36,12 @@ public class MessageService {
     public List<SessionMetaDTO> querySessionList(SessionQuery sessionQuery) throws BusinessException {
         LoginUser loginUser = ThreadContext.getLoginToken();
         UserProfileDTO userProfile = userProfileService.getByLoginUser(loginUser);
-        //Asserts.isTrue(!userProfile.getIsManager(), SparrowError.SYSTEM_PERMISSION_DENIED);
+        AuthenticatorConfigReader authenticatorConfigReader = ApplicationContext.getContainer().getBean(AuthenticatorConfigReader.class);
+        int platformId = authenticatorConfigReader.getPlatform();
+        boolean isAdmin =loginUser.getCategory().equals(platformId);
+        if (!isAdmin) {
+            sessionQuery.setUserId(userProfile.getUserId());
+        }
         return this.sessionMateRepository.querySessions(sessionQuery);
     }
 
